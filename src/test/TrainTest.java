35e6e1aebb7068ce37671ebeb168d9cc82edb7ba
paul <pmc.valentin@gmail.com>
2016-02-12 16:24:45 +0100

    Creation de la bonne classe test de train.

---
+++ src/test/TrainTest.java
@@ -0,0 +1,39 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class TrainTest {
+	@Before
+	public void setup(){
+		
+	}
+	
+	@Test
+	public void testTrain() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetCurrentCanton() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetCurrentCanton() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRun() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testToString() {
+		fail("Not yet implemented");
+	}
+
+}
