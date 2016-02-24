9c5b2b95000f77e5da7bd7b71288c664cfdd8e24
Bruno <bruno@cmitess.fr>
2016-02-24 18:29:12 +0100

    Line showed on the "Global view" window and added method to set buttons actions

--- src/ucp/greves/view/GlobalView.java
+++ src/ucp/greves/view/GlobalView.java
@@ -1,9 +1,14 @@
 package ucp.greves.view;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.Slider;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
@@ -20,17 +25,56 @@ public class GlobalView extends Application{
 	      Scene scene = new Scene(root,800,600);
 	      primaryStage.setTitle("G.R.E.V.E.S.");
 	      
-	      BorderPane lineDraw = (BorderPane) root.lookup("#lineDraw"); //Get the borderPane from the root
-	      //modify(lineDraw);
-	     
+	      ScrollPane lineDraw = (ScrollPane) root.lookup("#lineDraw"); //Get the borderPane from the root
+	      modify(lineDraw);
+	      setButton(root);
+	      
 	      primaryStage.setScene(scene);
 	      primaryStage.show();
 	     
 	}
 	
-	public void modify(BorderPane pane){
+	public void modify(ScrollPane pane){
 	    LineView lv = new LineView();
-	    pane.getChildren().add(lv);
+	    pane.setContent(lv);
+	}
+	
+	public void setButton(Parent root){
+		Button buttonAdd = (Button) root.lookup("#buttonAdd");
+		buttonAdd.setOnAction(new EventHandler<ActionEvent>() {
+			
+			@Override
+			public void handle(ActionEvent event) {
+				System.out.println("buttonAdd");	//Action to do
+			}
+		});
+		
+		Button buttonRemove = (Button) root.lookup("#buttonRemove");
+		buttonRemove.setOnAction(new EventHandler<ActionEvent>() {
+			
+			@Override
+			public void handle(ActionEvent event) {
+				System.out.println("buttonRemove");		//Action to do		
+			}
+		});
+		
+		Button buttonSlow = (Button) root.lookup("#buttonSlow");
+		buttonSlow.setOnAction(new EventHandler<ActionEvent>() {
+			
+			@Override
+			public void handle(ActionEvent event) {
+				System.out.println("buttonSlow");	//Action to do			
+			}
+		});
+		
+		Button buttonBlock = (Button) root.lookup("#buttonBlock");
+		buttonBlock.setOnAction(new EventHandler<ActionEvent>() {
+			
+			@Override
+			public void handle(ActionEvent event) {
+				System.out.println("buttonBlock");	//Action to do
+			}
+		});
 	}
 
 }
