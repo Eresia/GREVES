07ca408f14d8b13d2900ac35daf67ac82d5bb5d5
Eresia <bastien-12@hotmail.fr>
2016-02-25 23:08:39 +0100

    Ajout de la connaissance de la voie par le canton

---
+++ src/ucp/greves/model/exceptions/railway/RailWayNotDefinedException.java
@@ -0,0 +1,12 @@
+package ucp.greves.model.exceptions.railway;
+
+public class RailWayNotDefinedException extends Exception{
+
+	public RailWayNotDefinedException(){
+		super();
+	}
+	
+	public RailWayNotDefinedException(String s){
+		super(s);
+	}
+}
--- src/ucp/greves/model/line/canton/Canton.java
+++ src/ucp/greves/model/line/canton/Canton.java
@@ -4,6 +4,7 @@ import java.util.Observable;
 
 import ucp.greves.model.configuration.ConfigurationEnvironment;
 import ucp.greves.model.exceptions.canton.TerminusException;
+import ucp.greves.model.exceptions.railway.RailWayNotDefinedException;
 import ucp.greves.model.exceptions.station.StationNotFoundException;
 import ucp.greves.model.line.Line;
 import ucp.greves.model.line.RoadMap;
@@ -23,11 +24,16 @@ public class Canton extends Observable {
 
 	private StationDecorator station;
 	private int positionStation;
+	private int railWay;
+	
+	public Canton(Canton nextCanton, int railWay, int length) {
+		this(length);
+		this.nextCanton = nextCanton;
+		this.railWay = railWay;
+	}
 
 	public Canton(Canton nextCanton, int length) {
-		this.id = Line.register_canton(this);
-		this.nextCanton = nextCanton;
-		buildCanton(length);
+		this(nextCanton, -1, length);
 	}
 
 	protected Canton(int length) {
@@ -134,6 +140,17 @@ public class Canton extends Observable {
 	
 	public Station getStation() throws StationNotFoundException{
 		return station.getStation();
+	}
+	
+	public int getRailWay() throws RailWayNotDefinedException{
+		if(railWay == -1){
+			throw new RailWayNotDefinedException("RailWay is not define for canton " + getId());
+		}
+		return railWay;
+	}
+	
+	public void setRailWay(int railWay){
+		this.railWay = railWay;
 	}
 
 	@Override
