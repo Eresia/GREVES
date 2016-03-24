50f33ad4dfc76be326fa4899d04def4e7716a81a
paul <pmc.valentin@gmail.com>
2016-03-24 15:29:43 +0100

    Javadoc

--- src/ucp/greves/view/LineView.java
+++ src/ucp/greves/view/LineView.java
@@ -1,17 +1,24 @@
+/*
+ * TODO : JavaDoc
+ */
 package ucp.greves.view;
 
 import java.util.ArrayList;
 
-import ucp.greves.controller.GodModeController;
 import ucp.greves.controller.RailWayController;
-import ucp.greves.model.train.Train;
 import javafx.beans.property.IntegerProperty;
 import javafx.scene.Parent;
 
 public class LineView extends Parent{
 	
 	public ArrayList<RailWayView> RailWays;
-	
+	/**
+	 * Construct the view line with width and height
+	 * @param paneWidth
+	 * 	(IntegerProperty) the width of the pane
+	 * @param paneHeight
+	 * 	(IntegerProperty) The Height of the pane
+	 */
 	public LineView(IntegerProperty paneWidth, IntegerProperty paneHeight){
 
 		RailWays = new ArrayList<RailWayView>();
