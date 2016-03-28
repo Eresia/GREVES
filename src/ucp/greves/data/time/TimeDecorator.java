a8d903982278c40fd9b5d02089b960c67419767b
Agurato <vincent1611@hotmail.fr>
2016-03-28 17:13:43 +0200

    Javadoc Time

--- src/ucp/greves/data/time/TimeDecorator.java
+++ src/ucp/greves/data/time/TimeDecorator.java
@@ -2,6 +2,12 @@ package ucp.greves.data.time;
 
 import ucp.greves.model.exceptions.time.UndefinedTimeException;
 
+/**
+ * Decorator Pattern for Time
+ * 
+ * @see Time
+ * @see UndefinedTime
+ */
 public interface TimeDecorator {
 	
 	public String getString() throws UndefinedTimeException;
--- src/ucp/greves/data/time/UndefinedTime.java
+++ src/ucp/greves/data/time/UndefinedTime.java
@@ -2,8 +2,17 @@ package ucp.greves.data.time;
 
 import ucp.greves.model.exceptions.time.UndefinedTimeException;
 
+/**
+ * When the Time is undefined
+ * 
+ * @see Time
+ * @see TimeDecorator
+ */
 public class UndefinedTime implements TimeDecorator{
 	
+	/**
+	 * @throws UndefinedTimeException
+	 */
 	@Override
 	public String getString() throws UndefinedTimeException{
 		throw new UndefinedTimeException();
